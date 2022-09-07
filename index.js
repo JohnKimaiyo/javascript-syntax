ffb1cbf88a78b07018daa763040a973d640dfe9a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-09-07 08:45:29 +0300

    javascript syntax

---
+++ index.js
@@ -0,0 +1,48 @@
+// example 1
+let a = 5;
+let b = 5;
+let c = a+b;
+console.log('Answer:'+  c);
+
+// example 2
+let d = 10;
+let e = 10;
+let f = d+e;
+console.log('answer:' + f);
+
+// example 3
+
+let g = 15;
+let h = 15;
+let i = g + h;
+console.log('answer:' + i);
+
+// example 4
+let j = 20;
+let k = 20;
+let l = j + k;
+console.log('answer:' + l);
+
+// example 5
+let m = 25;
+let n = 25;
+let o = m + n ;
+console.log('answer:' + o);
+
+// example 6
+let p = 30;
+let q = 30;
+let r = p +q ;
+console.log('answer:' + r);
+
+// example 7
+let s = 35;
+let t = 35;
+let u = s + t;
+console.log('answer:' + u);
+
+// example 8
+let v = 40;
+let w = 40;
+let x = v +w;
+console.log('answer:' + x);
